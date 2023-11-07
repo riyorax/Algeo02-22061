b077d74d337fbae1f9efe7612566d84a7a2cadc5
riyorax <maximiliansulistiyo@gmail.com>
2023-11-07 15:25:14 +0700

    feat: add matrixRgb to matrixHSV

--- src/ImageToMatrix.py
+++ src/ImageToMatrix.py
@@ -5,9 +5,8 @@ def ImageToMatrix(path):
     img_bgr = cv2.imread(path)
     img_bgr = numpy.array(img_bgr)
     img_rgb = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2RGB)
+    img_rgb = img_rgb.astype('float64')
     return img_rgb
-
-# print(ImageToMatrix("waifu2.jpeg"))
 
 def RGBtoHSV(r,g,b):
     r = r/255
@@ -22,7 +21,7 @@ def RGBtoHSV(r,g,b):
         h = 60*(((g-b)/Delta)%6)
     elif Delta == g:
         h = 60*(((b-r)/Delta)+2)
-    elif Delta == b:
+    else:
         h = 60*(((r-g)/Delta)+4)
     
     if Cmax == 0:
@@ -37,4 +36,11 @@ def RGBtoHSV(r,g,b):
 def RGBtoGrayScale(r,g,b):
     return 0.29*r + 0.587*g + 0.114*b
 
-# def arrayRGBToarrayHSV(array_rgb):
+def matrixRGBTomatrixHSV(matrix_rgb):
+    for i in range(matrix_rgb.shape[0]):
+        for j in range(matrix_rgb.shape[1]):
+            r = matrix_rgb[i][j][0]
+            g = matrix_rgb[i][j][1]
+            b = matrix_rgb[i][j][2]
+            matrix_rgb[i][j] = RGBtoHSV(r,g,b)
+    return matrix_rgb
