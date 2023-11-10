fe7e85227163a74e9554649ebf43b0fa559e3b22
evelynnn04 <yosianaevelyn5@gmail.com>
2023-11-10 21:57:45 +0700

    feat: fix html, css, app. chore: add js

--- src/web/app.py
+++ src/web/app.py
@@ -1,42 +1,34 @@
-from flask import Flask, request, render_template
+from flask import Flask, render_template
+from PIL import Image
 import os
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = "uploaded_images"
-app.config['IMAGE_FOLDER'] = 'gallery'
-app.config['ALLOWED_EXTENSIONS'] = {'png, jpg, jpeg, csv'}
 
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
+def get_image_list(directory):
+    image_list = []
+    files = os.listdir(directory)
 
-@app.route("/index")
-def Index():
-    return render_template("index.html")
+    for file in files:
+        if file.lower().endswith(('.png', '.jpg', '.jpeg')):
+            image_path = os.path.join(directory, file)
+            image = Image.open(image_path)
+            image_list.append((file, image))
 
-@app.route('/', methods=['GET', 'POST'])
+    return image_list
+
+@app.route('/')
 def index():
-    if request.method == 'POST':
-        if 'image' not in request.files:
-            return render_template('index.html', message='No file part')
-        file = request.files['image']
-        if file.filename == '':
-            return render_template('index.html', message='No selected file')
-        file.save(app.config['UPLOAD_FOLDER'] + '/' + file.filename)
-        return render_template('index.html', message='File uploaded successfully')
-    return render_template('index.html', message=None)
+    # Use os.path.abspath to get the absolute path
+    directory_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "static/images"))
+    
+    # Ensure that the 'static/images' folder structure exists
+    if not os.path.exists(directory_path):
+        print(f"Error: The directory '{directory_path}' does not exist.")
+        return "Error: Image directory not found."
 
-@app.route('/upload', methods=['POST'])
-def upload_dataset():
-    if 'dataset' not in request.files:
-        return 'No file part'
-    file = request.files['dataset']
-    if file.filename == '':
-        return 'No selected file'
-    if file and allowed_file(file.filename):
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
-        return 'Dataset uploaded successfully!'
-    else:
-        return 'Invalid file format. Please upload a CSV file.'
+    images = get_image_list(directory_path)
 
-if __name__ == "__main__":
+    return render_template('index.html', images=images)
+
+if __name__ == '__main__':
     app.run(debug=True)
