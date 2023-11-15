5342fc8f3aa72e5fdb1f0294962dfcd599269483
riyorax <maximiliansulistiyo@gmail.com>
2023-11-15 13:41:09 +0700

    fix: app.py and html to upload files

--- src/app.py
+++ src/app.py
@@ -1,13 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for
 import os
 from werkzeug.utils import secure_filename
-
+import shutil
 
 app = Flask(__name__)
 
 SINGLE_UPLOAD_FOLDER = 'src/uploads/single_uploads'
 MULTIPLE_UPLOAD_FOLDER = 'src/uploads/multiple_uploads'
-ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'bmp'}
 
 app.config['SINGLE_UPLOAD_FOLDER'] = SINGLE_UPLOAD_FOLDER
 app.config['MULTIPLE_UPLOAD_FOLDER'] = MULTIPLE_UPLOAD_FOLDER
@@ -19,8 +19,42 @@ def allowed_file(filename):
 def index():
     return render_template('index.html')
 
+def empty_folder(folder_path):
+    if os.path.exists(folder_path):
+        for filename in os.listdir(folder_path):
+            file_path = os.path.join(folder_path, filename)
+            try:
+                if os.path.isfile(file_path) or os.path.islink(file_path):
+                    os.unlink(file_path)
+                elif os.path.isdir(file_path):
+                    shutil.rmtree(file_path)
+            except Exception as e:
+                print('Failed to delete %s. Reason: %s' % (file_path, e))
+
+# @app.route('/', methods=['POST'])
+# def upload_files():
+
+#     single_file = request.files.get('single_file')
+#     if single_file and allowed_file(single_file.filename):
+#         single_filename = os.path.join(app.config['SINGLE_UPLOAD_FOLDER'], secure_filename(single_file.filename))
+#         single_file.save(single_filename)
+
+#     multiple_files = request.files.getlist('multiple_files')
+#     for file in multiple_files:
+#         if file and allowed_file(file.filename):
+#             multiple_filename = os.path.join(app.config['MULTIPLE_UPLOAD_FOLDER'], secure_filename(file.filename))
+#             file.save(multiple_filename)
+
+#     return redirect(url_for('index'))
+
 @app.route('/', methods=['POST'])
 def upload_files():
+
+    empty_folder(app.config['SINGLE_UPLOAD_FOLDER'])
+    empty_folder(app.config['MULTIPLE_UPLOAD_FOLDER'])
+
+    os.makedirs(app.config['SINGLE_UPLOAD_FOLDER'], exist_ok=True)
+    os.makedirs(app.config['MULTIPLE_UPLOAD_FOLDER'], exist_ok=True)
 
     single_file = request.files.get('single_file')
     if single_file and allowed_file(single_file.filename):
@@ -33,7 +67,7 @@ def upload_files():
             multiple_filename = os.path.join(app.config['MULTIPLE_UPLOAD_FOLDER'], secure_filename(file.filename))
             file.save(multiple_filename)
 
-    return redirect(url_for('index'))
+    return redirect(url_for('result'))
 
 @app.route("/result.html")
 def result():
